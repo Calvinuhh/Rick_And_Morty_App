45a98df14945d73ec880d31a2a8ddf1a41c57ab5
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2024-01-13 17:49:02 -0500

    Primera parte

---
+++ src/components/Cards.jsx
@@ -0,0 +1,21 @@
+import Card from "./Card.jsx";
+
+export default function Cards({ characters }) {
+  return (
+    <div>
+      {characters.map((characters) => (
+        <Card
+          key={characters.id}
+          id={characters.id}
+          name={characters.name}
+          status={characters.status}
+          species={characters.species}
+          gender={characters.gender}
+          origin={characters.origin.name}
+          image={characters.image}
+          onClose={() => window.alert("Emulamos que se cierra la card")}
+        />
+      ))}
+    </div>
+  );
+}
